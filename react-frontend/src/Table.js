69f6fddb48dea29bc7c32cb6939c46d64d0aee9e
Matthew <mattbosio08@gmail.com>
2023-09-30 15:52:59 -0700

    Added state to Table.js and MyApp.js: :

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -6,6 +6,7 @@ function TableHeader() {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Delete</th>
       </tr>
     </thead>
   );
@@ -14,9 +15,15 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
-      </tr>
+	  <td>{row.name}</td>
+	  <td>{row.job}</td>
+	  <td>
+			<button onClick={() => 
+				props.removeCharacter(index)}>
+				Delete
+			</button>
+	  </td>
+	</tr>
     );
    }
   );
@@ -27,12 +34,14 @@ function TableBody(props) {
    );
 }
 
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} 
+	      removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
+
 export default Table;
