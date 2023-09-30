0a7be2ff36d0a5125804abb912e3dc74f404820d
Matthew <mattbosio08@gmail.com>
2023-09-30 16:02:47 -0700

    Added Form

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -15,31 +15,25 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-	  <td>{row.name}</td>
-	  <td>{row.job}</td>
-	  <td>
-			<button onClick={() => 
-				props.removeCharacter(index)}>
-				Delete
-			</button>
-	  </td>
-	</tr>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     );
-   }
-  );
-  return (
-      <tbody>
-        {rows}
-       </tbody>
-   );
+  });
+  return <tbody>{rows}</tbody>;
 }
 
-function Table (props) {
+function Table(props) {
   return (
     <table>
       <TableHeader />
-      <TableBody characterData={props.characterData} 
-	      removeCharacter={props.removeCharacter} />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
     </table>
   );
 }
