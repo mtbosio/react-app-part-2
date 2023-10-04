ec7ba6d7e0b7932f078201c73adfb0e206be2aec
Matthew <mattbosio08@gmail.com>
2023-10-04 10:01:31 -0700

    Added backend.js route for Get User Id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,9 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -50,7 +53,15 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
-
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
